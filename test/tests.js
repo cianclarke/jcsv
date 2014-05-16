8ea95ae9edb080fc38b01c6d5f335fbef1a3a7c2
cianclarke <cian@cianclarke.com>
2014-05-16 13:16:53 -0400

    Deal better with strings & objects, bump version

--- test/tests.js
+++ test/tests.js
@@ -5,10 +5,8 @@ data = require('./data.json');
 jcsv(data, { separator : ',', newline : ";\n", headers : true }, function(err, res){
   assert.ok(!err);
   assert.ok(typeof res === "string");
-  var resStr = "a,b,c;\n1,2,;\n3,4,5;\n";
-  assert.ok(res === resStr);
-
   console.log(res);
-
+  var resStr = ['a,b,c,d,e,f;', '1,2,,,,;', '3,4,5,,,;', '6,7,8,true,"string,[object Object];'];
+  assert.ok(res === resStr.join('\n') + '\n');
   console.log("Tests passed ok!");
 });
