fedf35fc5cc9ead7c567dd2f04adc57c01c110ff
cianclarke <cian@cianclarke.com>
2014-05-16 13:23:27 -0400

    String handling bug

--- test/tests.js
+++ test/tests.js
@@ -6,7 +6,7 @@ jcsv(data, { separator : ',', newline : ";\n", headers : true }, function(err, r
   assert.ok(!err);
   assert.ok(typeof res === "string");
   console.log(res);
-  var resStr = ['a,b,c,d,e,f;', '1,2,,,,;', '3,4,5,,,;', '6,7,8,true,"string,[object Object];'];
+  var resStr = ['a,b,c,d,e,f;', '1,2,,,,;', '3,4,5,,,;', '6,7,8,true,"string",[object Object];'];
   assert.ok(res === resStr.join('\n') + '\n');
   console.log("Tests passed ok!");
 });
